883c6a3085a6f3dd175be0d24f5d32a62f9f2002
mukul suyal <mukul01suyal@gmail.com>
2022-02-08 12:45:15 +0530

    first commit

---
+++ src/hemisphere.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import south from './Image/south.jpg'
+import north from './Image/north.jpg'
+
+
+
+    const Hemisphere= ({latitude})=>{
+        const hemisphere= latitude>0?'current Location: Northern Hemisphere !!' : 'You are currently in : Southern Hemisphere !!'
+        const picture = latitude>0 ? north : south
+        if(picture=== north){
+             return(
+        <div className="north">
+            <div className="inner">
+            <img src={picture} alt="" />
+            </div>
+            <h1> {hemisphere} </h1>
+        </div>
+    )
+
+        }
+        else{
+             return(
+        <div className="south">
+            <div className="inner">
+            <img src={picture} alt="" />
+            </div>
+            <h1> {hemisphere} </h1>
+        </div>
+    )
+        }
+   
+}
+export default Hemisphere
